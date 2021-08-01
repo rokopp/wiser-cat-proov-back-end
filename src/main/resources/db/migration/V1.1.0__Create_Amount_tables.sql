bc28355b542f689f988b74642703842b25fc46e1
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-08-01 14:57:24 +0300

    Added amount, title and date criteria tables

---
+++ src/main/resources/db/migration/V1.1.0__Create_Amount_tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS amount (
+                                            id              SERIAL PRIMARY KEY,
+                                            filter_id      INT NOT NULL,
+                                            compare_condition			VARCHAR(20) NOT NULL,
+    FOREIGN KEY (filter_id)
+    REFERENCES filter (id) ON DELETE CASCADE
+    );
+
+INSERT INTO amount(filter_id, compare_condition) VALUES
+((SELECT id from filter WHERE filter_name='first_filter'), 'More');
+
+INSERT INTO amount(filter_id, compare_condition) VALUES
+((SELECT id from filter WHERE filter_name='second_filter'), 'Less');
